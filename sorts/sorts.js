1a7073462ccf80848b9d2f0702de6a472b0a29a3
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-01-08 13:15:12 +0000

    more testing - rewrote a flawed test, sort algorithm still doesnt work

--- sorts/sorts.js
+++ sorts/sorts.js
@@ -1,4 +1,16 @@
 function sorts(array) {
+  if (!Array.isArray(array)) {
+    return "I can only sort an array!"
+  } else if (array.length === 0) {
+    return "Nothing to sort!"
+  }
 
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i] < array[i-1]) {
+
+    }
+  }
+  return array
 }
+
 module.exports = sorts;
