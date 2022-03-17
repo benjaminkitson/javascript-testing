53cdfe6c12dec9b5520d405a8be8151c977eb1f5
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-03-17 18:13:01 +0100

    added a test for returning an integer

--- doors/doors.test.js
+++ doors/doors.test.js
@@ -1,13 +1,17 @@
-const capitalise = require('./doors');
+const doors = require('./doors');
+
+test('it should return an integer', () => {
+  expect((typeof (doors(1)))).toBe('integer')
+});
 
 test('first kata example', () => {
   expect(doors(5).toBe(2))
-})
+});
 
 test('second kata example', () => {
   expect(doors(10).toBe(3))
-})
+});
 
 test('third kata example', () => {
   expect(doors(100).toBe(10))
-})
+});
