8389be32d7c20c9b912a8281e9bf894fc8d5c62e
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-03-17 18:49:18 +0100

    created a for loop that generates an array of falses of length n, created a second for loop that iterates through the doors array n times

--- doors/doors.js
+++ doors/doors.js
@@ -1,6 +1,21 @@
 function doors(number) {
   if (typeof number != "number") throw new Error('Argument must be an number.')
-  return number
+
+  let doorsArr = []
+
+  for (let i = 0; i < number; i++) {
+    doorsArr.push(false)
+  }
+
+  for (let j = 0; j < number; j++) {
+
+  }
+
+
+
+  return doorsArr
 }
+
+console.log(doors(5))
 
 module.exports = doors
